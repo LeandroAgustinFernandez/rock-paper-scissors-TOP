e92adb50765b0a6e90829e8590a060600b93f824
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2022-04-08 19:41:42 -0300

    Create function playerSelection

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 const pieces = ['rock','paper','scissors'];
 
-function computerPlay(){
+function computerSelection(){
     let index = Math.floor(Math.random()*3);
     return pieces[index];
 }
 
+function playerSelection(){
+    let index = prompt(`Select a number:
+    1- rock 2- paper 3- scissors`);
+    if (Number(index) > 3 || Number(index) == 'NaN') {
+        console.log('This is not an option.');
+        return false;
+    }else{
+        return pieces[index-1];
+    }
+}
+
+// console.log(playerSelection());
 // console.log(computerPlay());
