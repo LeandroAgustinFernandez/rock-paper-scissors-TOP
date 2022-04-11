603051ab03c740a8414f500599603ab6d8890580
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2022-04-11 15:11:52 -0300

    GamePlay FiveRound

--- js/script.js
+++ js/script.js
@@ -1,20 +1,75 @@
-const pieces = ['rock','paper','scissors'];
+const pieces = ["rock", "paper", "scissor"];
 
-function computerSelection(){
-    let index = Math.floor(Math.random()*3);
-    return pieces[index];
+function computerSelection() {
+  let index = Math.floor(Math.random() * 3);
+  return pieces[index];
 }
 
-function playerSelection(){
-    let index = prompt(`Select a number:
-    1- rock 2- paper 3- scissors`);
-    if (Number(index) > 3 || Number(index) == 'NaN') {
-        console.log('This is not an option.');
-        return false;
-    }else{
-        return pieces[index-1];
+function playerSelection() {
+  let input =
+    prompt(`Select your weapon!
+    Rock Paper Scissor`) || "Cancel";
+  console.log(input);
+  let validOption = pieces.find((item) => item === input.toLowerCase());
+  if (!validOption) {
+    console.log("This is not an option.");
+    return false;
+  } else {
+    return input;
+  }
+}
+
+function computerPlay(playerSelection, computerSelection) {
+  let computer = computerSelection;
+  let player = playerSelection;
+  if (computer === player) {
+    console.log("It's a tie!!!");
+    return 't';
+  } else if (computer == "rock") {
+    if (player == "paper") {
+      console.log("Player Win!!! Paper beats Rock!");
+      return 'p'
+    } else {
+      console.log("Oh! You Lose!!! Rock beats Scissor!");
+      return 'c'
     }
+  } else if (computer == "paper") {
+    if (player == "scissor") {
+      console.log("Player Win!!! Scissor beats Paper!");
+      return 'p'
+    } else {
+      console.log("Oh! You Lose!!! Peper beats Rock!");
+      return 'c'
+    }
+  } else if (computer == "scissor") {
+    if (player == "rock") {
+      console.log("Player Win!!! Rock beats Scissor!");
+      return 'p'
+    } else {
+      console.log("Oh! You Lose!!! Scissor beats Paper!");
+      return 'c'
+    }
+  }
+  console.log(`${computer} ${player}`);
 }
 
-// console.log(playerSelection());
-// console.log(computerPlay());
+function gamePlay(){
+  let score = {
+   player: 0,
+   computer: 0,
+   tie: 0
+  }
+  for (let i = 0; i < 5; i++) {
+      let res = computerPlay(playerSelection(), computerSelection());
+      if (res == 'p') {
+        score.player++;
+      } else if(res == 'c'){
+        score.computer++;
+      }else{
+        score.tie++;
+      }
+  }
+  return score;
+}
+
+console.log(gamePlay())
